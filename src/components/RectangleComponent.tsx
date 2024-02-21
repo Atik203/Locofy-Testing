bb5686b78a7d661405879ebd42954af62b89f7ed
Atik203 <Atik203>
2024-02-21 14:52:16 +0000

    First Commit from Locofy Builder

---
+++ src/components/RectangleComponent.tsx
@@ -0,0 +1,8 @@
+import { FunctionComponent } from "react";
+import styles from "./RectangleComponent.module.css";
+
+const RectangleComponent: FunctionComponent = () => {
+  return <div className={styles.groupChild} />;
+};
+
+export default RectangleComponent;
---
+++ src/components/RectangleComponent1.tsx
@@ -0,0 +1,8 @@
+import { FunctionComponent } from "react";
+import styles from "./RectangleComponent1.module.css";
+
+const RectangleComponent1: FunctionComponent = () => {
+  return <section className={styles.notLoggedInDesktopChild} />;
+};
+
+export default RectangleComponent1;
