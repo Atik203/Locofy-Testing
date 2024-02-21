e3be549007e2e4d8ecd598dd413a10bbe71f10f8
Atik203 <Atik203>
2024-02-21 14:58:30 +0000

    Pushed from Locofy Builder

--- src/components/RectangleComponent.js
+++ src/components/RectangleComponent.js
@@ -1,7 +1,7 @@
 import "./RectangleComponent.css";
 
 const RectangleComponent = () => {
-  return <div className="group-child51" />;
+  return <div className="group-child40" />;
 };
 
 export default RectangleComponent;
